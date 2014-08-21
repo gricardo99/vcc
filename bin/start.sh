0f9cb3344443d5f534fe3a9f897cf8ba940decec
Gabriel Ricardo <gabriel.ricardo@gmail.com>
2014-08-20 21:22:17 -0700

    fixed log path

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,6 @@
-q $VCT_WKSP_HOME/src/kdb/common/vct_proc.q -vct-load src/kdb/database/tick.q -vct-schema vct -vct-tplog $VCT_HDB </dev/null >$VCT_WKSP_LOG/tick.log 2>&1 &
+q $VCT_WKSP_HOME/src/kdb/common/vct_proc.q -vct-load src/kdb/database/tick.q -vct-schema vct -vct-tplog $VCT_HDB </dev/null >$VCT_WKSP_LOGS/tick.log 2>&1 &
 sleep 2
-q $VCT_HDB/vct -p 5012 </dev/null >$VCT_WKSP_LOG/hdb.log 2>&1 &
-q $VCT_WKSP_HOME/src/kdb/common/vct_proc.q -vct-load src/kdb/database/r.q  -vct-tick :5010 -vct-hdb :5012 -p 5011 </dev/null >$VCT_WKSP_LOG/rtd.log 2>&1 &
+q $VCT_HDB/vct -p 5012 </dev/null >$VCT_WKSP_LOGS/hdb.log 2>&1 &
+q $VCT_WKSP_HOME/src/kdb/common/vct_proc.q -vct-load src/kdb/database/r.q  -vct-tick :5010 -vct-hdb :5012 -p 5011 </dev/null >$VCT_WKSP_LOGS/rtd.log 2>&1 &
 sleep 2
-q $VCT_WKSP_HOME/src/kdb/common/vct_proc.q -vct-load src/kdb/exch/curlrest/curlexch.q -vct-tick :5010 </dev/null >$VCT_WKSP_LOG/exchcurl.log 2>&1 &
+q $VCT_WKSP_HOME/src/kdb/common/vct_proc.q -vct-load src/kdb/exch/curlrest/curlexch.q -vct-tick :5010 </dev/null >$VCT_WKSP_LOGS/exchcurl.log 2>&1 &
